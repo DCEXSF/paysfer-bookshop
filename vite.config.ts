fc25607c5308adddf7a804c8c38e9f50aa7b9993
S-A-T-Y-A <satyanandan846@gmail.com>
2025-10-27 19:49:20 -0400

    feat: add PWA support with vite-plugin-pwa and update package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt"],
+      manifest: {
+        name: "Paysfer Bookshop",
+        short_name: "Bookshop",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        description: "A super-fast bookshop!",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
